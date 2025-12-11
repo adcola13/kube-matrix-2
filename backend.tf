0a24494eefb060c631facd3cff4c49216298ab23
Adrina Colaco <adcola13@gmail.com>
2025-12-11 17:46:29 +0530

    Update S3 bucket and DynamoDB table names

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "br-terraform-state-20251211112450"
+    bucket         = "br-terraform-state-20251211111554"
     key            = "envs/dev/vpc/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "br-terraform-locks-20251211112450"
+    dynamodb_table = "br-terraform-locks-20251211111554"
     encrypt        = true
   }
 }
