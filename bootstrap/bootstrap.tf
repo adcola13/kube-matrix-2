7df6208759961d02dc546c0c1ebc1ed7bcf8e792
Adrina Colaco <adcola13@gmail.com>
2025-12-11 15:15:00 +0530

    Update bootstrap.tf

--- bootstrap/bootstrap.tf
+++ bootstrap/bootstrap.tf
@@ -30,6 +30,7 @@ resource "time_static" "s3_suffix" {}
 locals {
   bucket_suffix = regexreplace(timestamp(), "[^0-9]", "")
   unique_bucket_name = "${var.bucket_prefix}-${local.bucket_suffix}"
+  unique_dynamo_name = "var.dynamodb_table_name-${local.bucket_suffix}"
 }
 
 ###############################################################################
@@ -77,7 +78,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "state_encrypt" {
 # 2. DYNAMODB TABLE FOR STATE LOCKING
 ###############################################################################
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = var.dynamodb_table_name
+  name         = local.unique_dynamo_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
