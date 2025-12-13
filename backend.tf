97424573552b2e2cd88dc8dc450886095d08c9ea
Adrina Colaco <adcola13@gmail.com>
2025-12-13 20:15:45 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "km-terraform-state-dev-20251212053538"
+    bucket         = "km-terraform-state-dev-20251213042220"
     key            = "envs/dev/vpc/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "km-terraform-locks-dev-20251212053538"
+    dynamodb_table = "km-terraform-locks-dev-20251213042220"
     encrypt        = true
   }
 }
