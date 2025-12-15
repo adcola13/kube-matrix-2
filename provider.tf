c0876222677ecb77d78d6b5d6f1c6c402db2169d
Adrina Colaco <adcola13@gmail.com>
2025-12-15 23:19:52 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -17,10 +17,6 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
-/*  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token */
-
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
